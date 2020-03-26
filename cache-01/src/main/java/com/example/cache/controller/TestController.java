bb38e8f77892d63813d82f2de1b51f0c10f88ae3
fangyulong253711 <3095329264@qq.com>
2020-03-26 16:32:44 +0800

    caching进行多个缓存

--- cache-01/src/main/java/com/example/cache/controller/TestController.java
+++ cache-01/src/main/java/com/example/cache/controller/TestController.java
@@ -3,8 +3,10 @@ package com.example.cache.controller;
 import com.example.cache.Bean.sp_type;
 import com.example.cache.mapper.sp_tpeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,10 +45,55 @@ public class TestController {
     }
 
 
+    /**
+     *
+     * @param sp_type
+     * @return
+     * CachePut同步更新缓存
+     *   调用之后删除之前的缓存，更新现在的缓存
+     */
     @GetMapping("/emp/update")
     @CachePut(value = "emp",key = "#result.type_id")
     public sp_type updateType(sp_type sp_type){
         sp_tpeMapper.updatetype(sp_type);
         return sp_type;
     }
+
+
+    /**
+     * CacheEvict删除缓存
+     *     参数：
+     *          allEntries = true 删除所有数据
+     *          beforeInvocation = true  方法是否在缓存清除之前执行
+     */
+    @GetMapping("/delemp/{id}")
+    @CacheEvict(value = "emp",key = "#id",allEntries = true,beforeInvocation = true)
+    public void deleteEmp(@PathVariable("id")  Integer id){
+        System.out.println("deleteEmp"+id);
+    }
+
+
+    /**
+     *Caching 指定多个进行缓存
+     */
+    @Caching(
+            cacheable = {
+                    @Cacheable(value = "emp",key = "#id")
+            },
+            put = {
+                    @CachePut(value = "emp",key = "#result.delete_time"),
+                    @CachePut(value = "emp",key = "#result.type_name")
+            }
+    )
+    @GetMapping("/emp2/{id}")
+    public sp_type getType2(@PathVariable("id") Integer id){
+        System.out.println("查询员工");
+        System.out.println(sp_tpeMapper.getTypeByid(id));
+        return  sp_tpeMapper.getTypeByid(id);
+    }
+
+
+
+
+
 }
